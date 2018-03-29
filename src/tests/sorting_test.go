b7e160be0b5378482ea3bdb0320605bb46c7c3cf
Vlad Bagdey <Vladislav_Bagdei@epam.com>
2018-03-29 23:30:49 +0300

    upd

--- src/tests/sorting_test.go
+++ src/tests/sorting_test.go
@@ -7,14 +7,14 @@ import (
 
 //TestQuickSort is working well
 func TestQuickSort(t *testing.T) {
-	array := []int{5, 6, 10, 12, 3, 4}
-	sortArray := []int{3, 4, 5, 6, 10, 12}
-	quickSort(array)
-	if len(array) != len(sortArray) {
+	actual := []int{5, 6, 10, 12, 3, 4}
+	expected := []int{3, 4, 5, 6, 10, 12}
+	quickSort(actual)
+	if len(actual) != len(expected) {
 		t.Error("Length not equals")
 	}
-	for index, value := range array {
-		if value != sortArray[index] {
+	for index, value := range actual {
+		if value != expected[index] {
 			t.Error("Quick sort error. Not working!")
 		}
 	}
