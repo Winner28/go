9e88ff47f30ee0727c7750a80e711cf5e722913e
Vlad Bagdey <Vladislav_Bagdei@epam.com>
2018-03-29 22:38:18 +0300

    testing env

--- src/main.go
+++ src/main.go
@@ -1,19 +1,42 @@
 package main
 
-import "algos"
+import (
+	"algos"
+	"fmt"
+	"time"
+)
 
-//Saiyan struct
-type Saiyan struct {
-	Name  string
-	Power int
-}
-
-func construct(name string, power int) Saiyan {
-	return Saiyan{name, power}
-}
-
-func main() {
+func workWithStack() {
+	fmt.Println("Work with Stack!")
 	stack := algos.InitStack([]int{1, 2, 3, 4}, "Regular stack!")
 	stack.Push(10)
 	algos.PrintStack(stack)
+
+}
+
+func count(routine string) {
+	for i := 0; i < 4; i++ {
+		fmt.Println(i, routine)
+		time.Sleep(time.Second * 1)
+	}
+}
+
+func printCount(c chan int) {
+	num := 0
+	for num >= 0 {
+		num = <-c
+		fmt.Println(num)
+	}
+}
+
+func main() {
+	fmt.Println("...Entry point to main function...")
+	c := make(chan int)
+	a := []int{1, 2, 3, 4, 5}
+	go printCount(c)
+	for _, value := range a {
+		c <- value
+	}
+	time.Sleep(time.Second * 33)
+
 }
