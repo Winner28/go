e85eb160fdbedea4775862782fe70c216e1759a2
Vlad Bagdey <Vladislav_Bagdei@epam.com>
2018-05-11 00:00:09 +0300

    working with gin

--- src/web/server/server.go
+++ src/web/server/server.go
@@ -1,9 +1,13 @@
 package server
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+)
 
 type server struct {
 	router *gin.Engine
+	db     *gorm.DB
 }
 
 var s *server
@@ -11,5 +15,14 @@ var s *server
 func init() {
 	s = &server{
 		router: gin.Default(),
+		db:     getDB(),
 	}
+}
+
+func (s *server) routes() {
+	bookRoutes := s.router.Group("/books")
+}
+
+func (s *server) setUpBookRoutes(bookRoutes *gin.RouterGroup) {
+
 }
