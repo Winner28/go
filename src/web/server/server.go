2b9718068d01077714b8bb38afa23ad1dcc29156
Vlad Bagdey <Vladislav_Bagdei@epam.com>
2018-05-11 00:10:53 +0300

    added bookRoutes

--- src/web/server/server.go
+++ src/web/server/server.go
@@ -21,8 +21,33 @@ func init() {
 
 func (s *server) routes() {
 	bookRoutes := s.router.Group("/books")
+	s.setUpBookRoutes(bookRoutes)
 }
 
 func (s *server) setUpBookRoutes(bookRoutes *gin.RouterGroup) {
+	bookRoutes.POST("/", createBook)
+	bookRoutes.GET("/", fetchBooks)
+	bookRoutes.GET("/:id", fetchBook)
+	bookRoutes.PUT("/:id", updateBook)
+	bookRoutes.DELETE("/:id", deleteBook)
+}
+
+func createBook(c *gin.Context) {
+
+}
+
+func fetchBooks(c *gin.Context) {
+
+}
+
+func fetchBook(c *gin.Context) {
+
+}
+
+func updateBook(c *gin.Context) {
+
+}
+
+func deleteBook(c *gin.Context) {
 
 }
